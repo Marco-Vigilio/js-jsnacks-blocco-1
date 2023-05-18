99468515b7047fb2fa6fec701b78ab6b94c1e7c6
Marco-Vigilio <tonyvigilio@gmail.com>
2023-05-18 16:35:12 +0200

    snack4 con inserimento del numero dall'utente

--- jsnack4/myscript.js
+++ jsnack4/myscript.js
@@ -4,26 +4,37 @@ Chiedi un numero di 4 cifre all’utente
 e calcola la somma di tutte le cifre che compongono il numero.
 */
 
-let numero = "1212";
+//let numero = "1212";
 let cifra;
 let somma = 0;
 let array = [];
 
-//let numero = parseInt(prompt("Inserisci un numeo da 4 cifre: "));
-    
-    for(let i = 0; i < numero.length; i++){
+let numero = prompt("Inserisci un numero da 4 cifre: ");
+
+let num = numero.length;
+
+if(isNaN(numero)){ //NON METTENDO parseInt deve essere per forza un numero la stringa
+    alert("Inserire un numero di 4 cifre, ricaricare la pagina");
+}
+else{
+    if((num > 0) && (num < 5)){
+        for(let i = 0; i < num; i++){
 
         
 
-        cifra = parseInt(numero.substring(i,(i+1)));
-        array[i] = cifra;
-        console.log(array[i]);
-        somma = somma + cifra;
+            cifra = parseInt(numero.substring(i,(i+1)));
+            array[i] = cifra;
+            console.log(array[i]);
+            somma = somma + cifra;
+        }
+    
+        console.log("La somma è: " + somma);
+    
+        let p = document.querySelector("#risultato");
+    
+        p.innerHTML = "La somma dei numeri tra loro è: " + somma;
     }
-
-    console.log("La somma è: " + somma);
-
-    let p = document.querySelector("#risultato");
-
-    p.innerHTML = "La somma dei numeri è: " + somma;
-
+    else{
+        alert("Inserisci un numero positivo da 4");
+    }
+}
