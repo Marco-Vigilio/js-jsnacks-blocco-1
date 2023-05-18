b5f9d41c1831f77347c1fa6d1a932d9a5dec0182
Marco-Vigilio <tonyvigilio@gmail.com>
2023-05-18 15:22:15 +0200

    snack3 ridimansionamento dell'array in base se Nan

---
+++ jsnack3/myscript.js
@@ -0,0 +1,51 @@
+/*
+Snack 3:
+*Crea un array vuoto.
+*Chiedi per 6 volte all’utente di inserire un numero,  
+se è dispari inseriscilo nell’array.
+*/
+
+let array = [];
+let numUtente;
+let p = document.querySelector("#risultato");
+
+p.innerHTML += "L'array contiene i seguenti numeri: ";
+for(let i = 0; i < 6; i++){
+
+    numUtente = parseInt(prompt("Inserisci " + (i+1) + "° numero"));
+
+    if(isNaN(numUtente)){
+        console.log("Il numero " + (i+1) + " non è stato inserito");
+        /*
+        array[i] = [];
+        console.log(array[i]);
+        
+        p.innerHTML += "[ ]  " ;
+        */
+    }
+    else{
+
+        if((numUtente % 2) === 0){
+            console.log("Il numero " + (i+1) + " è pari");
+            /*
+            array = [i];
+            array = [];
+            
+            console.log(array);
+            p.innerHTML += "[ ]  " ;
+            */
+            
+        }
+        else{
+            console.log("Il numero "+ (i+1) + " è dispari = " + array[i]);
+            array[i] = numUtente;
+            console.log(array[i]);
+            
+            p.innerHTML += "[" + array[i] + "]  " ;
+        }
+    }
+}
+
+
+console.log("La grandezza del mio array è: " + array.length);
+
